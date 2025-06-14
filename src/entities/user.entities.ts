bd927fffc2ae070594b01cfbc1c019edf4039bfa
unkv0wn <cjeanb0@gmail.com>
2025-06-14 11:44:37 -0300

    ✨ Adicionando model users

---
+++ src/entities/user.entities.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { eRoles } from 'src/config/enums/roles.enum';
+
+@Entity('users')
+export class mUser {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: 'enum', enum: eRoles, array: true, default: [eRoles.USER] })
+  roles: eRoles[];
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+}
